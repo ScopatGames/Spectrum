1d12763d0f354ab8fb959a993144cbd965ba9f57
Scott Patten <scopatten@gmail.com>
2016-06-30 09:42:52 -0600

    another day

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : NetworkBehaviour {
 
-	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
